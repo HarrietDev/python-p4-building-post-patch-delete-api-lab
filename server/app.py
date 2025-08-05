5c8885f038aa5fc636927eb77a453ddc7294c7ab
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-08-06 01:42:10 +0300

    Implement POST, PATCH, and DELETE routes for baked_goods and bakeries

--- server/app.py
+++ server/app.py
@@ -23,12 +23,17 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods =['GET','PATCH'])
 def bakery_by_id(id):
 
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    # bakery = Bakery.query.filter_by(id=id).first()
+    # bakery_serialized = bakery.to_dict()
+    bakery = Bakery.query.get_or_404(id)
+    if request.method == 'PATCH':
+        if 'name' in request.form:
+            bakery.name = request.form['name']
+            db.session.commit()
+    return make_response ( bakery.to_dict(), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -44,6 +49,39 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods', methods = ['POST'])
+def create_baked_good():
+    name = request.form.get('name')
+    price = request.form.get('price')
+    bakery_id = request.form.get('bakery_id')
+
+    if not name or not price:
+        return make_response({"error": "Name,price and bakery_id are required."}, 400)
+    
+    new_baked_good = BakedGood(
+        name =name, 
+        price=float(price),
+        bakery_id = int(bakery_id)
+        )
+
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    return make_response(new_baked_good.to_dict(), 201)
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get_or_404(id)
+
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    return make_response(
+        {"message": f"Baked good with ID {id} was successfully deleted."},
+        200
+    )
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
